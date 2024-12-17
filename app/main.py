0fd0dffa105f3d8958701535f0ef51f648c16ee0
Abdullah Shaik <abdullahshaik@MacBookPro.cable.rcn.com>
2024-12-17 15:54:52 -0600

    routers

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ import time
 from  sqlalchemy.orm import Session
 from . import models, schemas, utils
 from .database import engine, SessionLocal, get_db
-
+from .routers import post, user
    
 models.Base.metadata.create_all(bind=engine)
 
@@ -50,100 +50,11 @@ def find_index_post(id):
             return i
         
 
-app = FastAPI()
 
+app.include_router(post.router)
+app.include_router(user.router)
 
 @app.get("/")
 def root():
     return {"message": "Welcome to my apii"}
 
-@app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return posts
-
-@app.get("/posts", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts """)
-    # posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
-    return  posts
-
-@app.post("/posts", status_code= status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post:schemas.PostCreate, db: Session = Depends(get_db)):
-    # """post_dict = new_post.model_dump() # model_dump() converts the type Post in to dictionary.
-    # post_dict['id'] = randrange(1,1000000)
-    # my_posts.append(post_dict)
-    # print(new_post)
-    # print(post_dict)"""
-    # cursor.execute(""" INSERT INTO posts(title, content, published) VALUES (%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-    # conn.commit()
-  
-    new_post = models.Post(**post.model_dump())
-    print(new_post)
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    return new_post
-
-@app.get("/posts/{id}", response_model=schemas.Post)
-def get_post(id:int, db: Session = Depends(get_db)):
-   # post = find_post(id)
-    # cursor.execute(""" SELECT * FROM posts WHERE id =%s""",(str(id)))
-    # post = cursor.fetchone()
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-    
-   
-    if not post :
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail = f"post with id: {id} was not found")
-        #response.status_code = status.HTTP_404_NOT_FOUND
-        #return {"message": f"post with id: {id} was not found"}
-    
-    return post
-
-#delete post
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING*""",(str(id)))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
-
-    post = db.query(models.Post).filter(models.Post.id == id)
-    
-   
-
-    if post.first() == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="post with id : {id} does not exist")
-    post.delete(synchronize_session=False)
-    db.commit()
-    
-    return Response(status_code = status.HTTP_204_NO_CONTENT)
-
-
-@app.put("/posts/{id}")
-def update_post(id:int, post:schemas.PostCreate, db: Session = Depends(get_db), response_model=schemas.Post):
-    # cursor.execute(""" UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",(post.title, post.content, post.published,str(id)))
-    # updated_post = cursor.fetchone()
-    # conn.commit()
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    post_f = post_query.first()
-    print(post)
-    if post_f == None:
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail = f"post with id : {id} does not exist")
-    post_query.update(post.model_dump(), synchronize_session=False)
-    db.commit()
-    return post_query.first()
-
-
-@app.post("/users", status_code= status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_user(user:schemas.UserCreate, db: Session = Depends(get_db)):
-    # hash a password
-    hashed_password = utils.has(user.password)
-    user.password = hashed_password
-    new_user = models.User(**user.model_dump())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
